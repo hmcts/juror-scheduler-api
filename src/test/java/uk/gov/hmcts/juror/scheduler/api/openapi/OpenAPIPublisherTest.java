d2df7cc64a64e5ac34b04219f59905590253687d
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-05 16:29:11 +0100

    revert ignore change

--- src/test/java/uk/gov/hmcts/juror/scheduler/api/openapi/OpenAPIPublisherTest.java
+++ src/test/java/uk/gov/hmcts/juror/scheduler/api/openapi/OpenAPIPublisherTest.java
@@ -25,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Ignore
 class OpenAPIPublisherTest {
 
     @Autowired
