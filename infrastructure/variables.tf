09ecce47ed3db9e43fd4600daf32f2063db16b67
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-09 10:11:02 +0100

    add postgres db

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,16 +1,39 @@
-variable "product" {}
+#Variables with values passed in by Jenkins
 
-variable "component" {}
-
-variable "location" {
-  default = "UK South"
+variable "env" {
+  description = "The deployment environment (sandbox, aat, prod etc..)"
 }
 
-variable "env" {}
+variable "product" {
+  description = "The name of the application"
+}
 
-variable "subscription" {}
+variable "component" {
+  description = "The name of the microservice"
+}
+
+variable "subscription" {
+  description = "The subscription ID"
+}
+
+variable "aks_subscription_id" {
+  description = "The aks subscription ID"
+}
+
+variable "tenant_id" {
+  description = "The Azure AD tenant ID for authenticating to key vault"
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "The object ID of the user to be granted access to the key vault"
+}
 
 variable "common_tags" {
   type = map(string)
 }
 
+# Variables with default values or values specified in an {env}.tfvars file
+
+variable "location" {
+  default = "UK South"
+}
