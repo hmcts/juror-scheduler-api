984b3290c57e079570cb54197b2c27fa56a348bc
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-12 14:53:27 +0100

    add import

--- src/integrationTest/java/uk/gov/hmcts/juror/scheduler/openapi/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/juror/scheduler/openapi/OpenAPIPublisherTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
