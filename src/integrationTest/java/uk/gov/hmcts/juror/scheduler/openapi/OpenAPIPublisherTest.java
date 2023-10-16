2ccc5c175a865c42b5bb6c5db9bbc77f9b525be3
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-16 11:27:04 +0100

    redo profile

--- src/integrationTest/java/uk/gov/hmcts/juror/scheduler/openapi/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/juror/scheduler/openapi/OpenAPIPublisherTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.juror.standard.components.AdminAccountCreator;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles({"intTest", "h2db"})
 class OpenAPIPublisherTest {
 
     @Autowired
