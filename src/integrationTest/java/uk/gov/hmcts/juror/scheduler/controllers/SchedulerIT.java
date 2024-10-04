d7b5ed242fabaf8289b5dcaf5d505139ccb977cf
akikrahman1 <akhlaqur.rahman1@hmcts.net>
2024-10-04 10:42:35 +0100

    Update SchedulerIT.java

--- src/integrationTest/java/uk/gov/hmcts/juror/scheduler/controllers/SchedulerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/juror/scheduler/controllers/SchedulerIT.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.juror.scheduler.controllers;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
